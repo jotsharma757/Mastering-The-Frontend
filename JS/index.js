c0877c77efc9bb3d8478b79d2df2254e85b8668f
Harjot Kaur <harjotkaur6677@gmail.com>
2024-06-21 21:04:51 +0530

    HTML CSS Bootstrap

---
+++ JS/index.js
@@ -0,0 +1,58 @@
+// var let const 
+// let  a = 30 
+ 
+//   let b = 8
+ 
+
+//*****data type***** 
+
+//----PRIMITIVE DATATYPE
+// let str= "5465"
+//  console.log(str);
+// console.log(typeof(str));
+
+//----ARRAY------NON-PRIMITIVE DT
+// let arr = [ 12 ,"ERTR",true , undefined , null] 
+// console.log(arr[1]);
+
+
+//-----OBJECT---NON-PRIMITIVE DT
+// let obj={ 
+//     name : "Harjot",
+// role : "developer"
+// }
+// console.log(obj.role);
+// console.log(document);
+
+//-----TYPE OF OPERATOR----
+
+//  let bigint=12345678901234567890 
+//  console.log(bigint);
+//  console.log(typeof(bigint));
+
+//  let un
+  // console.log(un);
+  // var un = " 5"
+  // console.log(typeof(un));
+
+  let arr = [0,1,2,3,4,5,6,7,89,9,0,4,565,654,65,]
+
+  //----POP METHOD----
+  //  let rr = arr.pop()
+  //  console.log(rr);
+
+
+   //---PUSH METHOD---
+  //   arr.push(46)
+  // console.log(arr);
+
+
+   //---SHIFT METHOD---means right side toh element pop hojegga
+  //  arr.shift()
+  // console.log(arr);
+
+
+   //---SPLICE METHOD----means arr.slice(start,delete.counting,kheda element add krwana)
+// arr.splice(4 ,2,9)
+// console.log(arr);
+
---
+++ JS/tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+arr.splice(4 ,2)
